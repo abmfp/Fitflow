d6240cc1ed56b1e84c4eded0d6c422b869323a46
abmfp <abmfp2@gmail.com>
2025-09-17 16:25:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,42 @@
 import 'package:fitflow/screens/nav_screen.dart';
+import 'package:fitflow/services/user_service.dart';
+import 'package:fitflow/services/weight_service.dart';
+import 'package:fitflow/services/workout_service.dart';
 import 'package:fitflow/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  // Ensure Flutter is ready
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Hive for local database storage
+  await Hive.initFlutter();
+
+  // Register our custom object adapters for Hive
+  Hive.registerAdapter(WeightEntryAdapter());
+  Hive.registerAdapter(ExerciseAdapter());
+  Hive.registerAdapter(CustomExerciseAdapter());
+
+  // Open all the boxes we need for our services
+  await Hive.openBox('user_data');
+  await Hive.openBox<WeightEntry>('weight_history');
+  await Hive.openBox('workout_data');
+  await Hive.openBox<CustomExercise>('custom_exercises');
+  await Hive.openBox<List>('workout_history'); // Box to store lists of exercises
+
+  // Initialize services to load data from the database
+  await UserService().init();
+  await WeightService().init();
+  await WorkoutService().init();
+
+  // Lock screen orientation to portrait
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  
   runApp(const MyApp());
 }
 
