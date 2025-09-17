02021f0f130dd77949d4047189cdf91ce122f005
abmfp <abmfp2@gmail.com>
2025-09-17 14:48:03 +0530

    Create nav_screen.dart

---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,58 @@
+import 'package:fitflow/screens/home_screen.dart';
+import 'package:fitflow/screens/plan_screen.dart';
+import 'package:fitflow/screens/profile_screen.dart';
+import 'package:fitflow/screens/progress_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const ProgressScreen(),
+    const PlanScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Progress',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Plan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
