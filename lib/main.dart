584db0de36b22b308b20df98dffeea7cbfb37935
abmfp <abmfp2@gmail.com>
2025-09-21 10:56:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,24 @@ import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
-  // Ensure Flutter is ready
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Initialize Hive for local database storage
   await Hive.initFlutter();
 
-  // Register our custom object adapters for Hive
   Hive.registerAdapter(WeightEntryAdapter());
   Hive.registerAdapter(ExerciseAdapter());
   Hive.registerAdapter(CustomExerciseAdapter());
 
-  // Open all the boxes we need for our services
   await Hive.openBox('user_data');
   await Hive.openBox<WeightEntry>('weight_history');
   await Hive.openBox('workout_data');
   await Hive.openBox<CustomExercise>('custom_exercises');
-  await Hive.openBox<List>('workout_history'); // Box to store lists of exercises
+  await Hive.openBox<List>('workout_history');
 
-  // Initialize services to load data from the database
   await UserService().init();
   await WeightService().init();
   await WorkoutService().init();
 
-  // Lock screen orientation to portrait
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
