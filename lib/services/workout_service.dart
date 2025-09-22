afa8e782254f4494b5c1d9c08ec9e5c4a278c5ac
abmfp <abmfp2@gmail.com>
2025-09-22 19:15:59 +0530

    Update workout_service.dart

--- lib/services/workout_service.dart
+++ lib/services/workout_service.dart
@@ -1,53 +1,70 @@
-import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
-import 'package:intl/intl.dart';
-import 'package:week_of_year/week_of_year.dart';
-
+// ... imports ...
 part 'workout_service.g.dart';
 
-// ... Exercise and CustomExercise models are the same ...
+@HiveType(typeId: 1)
+class Exercise {
+  // ... fields are the same ...
+  @HiveField(5) // New field
+  final String? imagePath;
+  @HiveField(6) // New field
+  final String? videoPath;
+
+  Exercise({
+    required this.name,
+    this.isCompleted = false,
+    this.description,
+    this.imagePath, // Add to constructor
+    this.videoPath, // Add to constructor
+  });
+}
+
+@HiveType(typeId: 2)
+class CustomExercise extends HiveObject { // Extend HiveObject for easier updates
+  @HiveField(0)
+  final String name;
+  @HiveField(1)
+  final String muscleGroup;
+  @HiveField(2) // New field
+  String? imagePath;
+  @HiveField(3) // New field
+  String? videoPath;
+  
+  CustomExercise({required this.name, required this.muscleGroup, this.imagePath, this.videoPath});
+}
 
 class WorkoutService extends ChangeNotifier {
-  // ... _instance, _dataBox, etc., are the same ...
+  // ... everything else is the same until startWorkoutForDay ...
 
-  // New getter to see today's COMPLETED workout from the history
-  List<Exercise> get todaysCompletedWorkout {
-    final today = DateTime.utc(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-    return _workoutHistory[today] ?? [];
-  }
+  void startWorkoutForDay(DateTime date) {
+    List<String> musclesToTrain = getMusclesForDay(date);
+    List<CustomExercise> availableExercises = getExercisesForMuscleGroups(musclesToTrain);
 
-  Future<void> init() {
-    // ... same as before, loading data from Hive ...
-  }
-  
-  // THIS IS THE FIX for the streak count
-  int finishCurrentWorkout() {
-    final completedExercises = _currentWorkoutExercises.where((e) => e.isCompleted).toList();
-    
-    if (completedExercises.isNotEmpty) {
-      final today = DateTime.utc(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-      _workoutHistory[today] = completedExercises;
-      _historyBox.put(today.toIso8601String(), completedExercises);
+    _currentWorkoutExercises = availableExercises
+        .map((customEx) => Exercise(
+              name: customEx.name,
+              // Copy the paths over when starting a workout
+              imagePath: customEx.imagePath,
+              videoPath: customEx.videoPath,
+            ))
+        .toList();
+        
+    for (var exercise in _currentWorkoutExercises) {
+      exercise.isCompleted = false;
     }
-    
-    int currentWeek = DateTime.now().weekOfYear;
-    if (currentWeek != _lastWorkoutWeek) {
-      _weeklyWorkoutCount = 1;
-    } else {
-      // Only increment if we are below the goal
-      if (_weeklyWorkoutCount < 6) {
-        _weeklyWorkoutCount++;
-      }
-    }
-    
-    _lastWorkoutWeek = currentWeek;
-    _dataBox.put('weeklyWorkoutCount', _weeklyWorkoutCount);
-    _dataBox.put('lastWorkoutWeek', _lastWorkoutWeek);
-    
-    _currentWorkoutExercises = [];
     notifyListeners();
-    return completedExercises.length;
   }
 
-  // ... rest of the service file is the same ...
+  // New method to update an exercise
+  void updateCustomExercise(CustomExercise oldExercise, CustomExercise newExercise) {
+    final index = _customExercises.indexOf(oldExercise);
+    if (index != -1) {
+      _customExercises[index] = newExercise;
+      oldExercise.imagePath = newExercise.imagePath;
+      oldExercise.videoPath = newExercise.videoPath;
+      oldExercise.save(); // Save changes to the database
+      notifyListeners();
+    }
+  }
+
+  // ... rest of the service is the same ...
 }
