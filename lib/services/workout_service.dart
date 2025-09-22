80fca6b34f76926b8d00f8319e80d3df0f36f666
abmfp <abmfp2@gmail.com>
2025-09-22 17:42:21 +0530

    Update workout_service.dart

--- lib/services/workout_service.dart
+++ lib/services/workout_service.dart
@@ -6,12 +6,33 @@ part 'workout_service.g.dart';
 
 @HiveType(typeId: 1)
 class Exercise {
-  // ... (content of Exercise class is the same)
+  @HiveField(0)
+  final String name;
+  @HiveField(1)
+  bool isCompleted;
+  @HiveField(2)
+  final String? description;
+  @HiveField(3)
+  final String? imageUrl;
+  @HiveField(4)
+  final String? videoUrl;
+
+  Exercise({
+    required this.name,
+    this.isCompleted = false,
+    this.description,
+    this.imageUrl,
+    this.videoUrl,
+  });
 }
 
 @HiveType(typeId: 2)
 class CustomExercise {
-  // ... (content of CustomExercise class is the same)
+  @HiveField(0)
+  final String name;
+  @HiveField(1)
+  final String muscleGroup;
+  CustomExercise({required this.name, required this.muscleGroup});
 }
 
 class WorkoutService extends ChangeNotifier {
@@ -23,21 +44,44 @@ class WorkoutService extends ChangeNotifier {
   late Box<List> _historyBox;
   late Box<CustomExercise> _customExercisesBox;
 
-  // ... (weeklyPlan, currentWorkoutExercises, etc. are the same)
-  Map<DateTime, List<Exercise>> _workoutHistory = {};
+  final List<Map<String, dynamic>> _weeklyPlan = [
+    {'day': 'Monday', 'muscles': ['Chest', 'Biceps']},
+    {'day': 'Tuesday', 'muscles': ['Back', 'Triceps']},
+    {'day': 'Wednesday', 'muscles': ['Legs', 'Shoulders']},
+    {'day': 'Thursday', 'muscles': []},
+    {'day': 'Friday', 'muscles': ['Chest', 'Back']},
+    {'day': 'Saturday', 'muscles': ['Abs']},
+    {'day': 'Sunday', 'muscles': []},
+  ];
   
-  // ... (getters are the same)
+  Map<DateTime, List<Exercise>> _workoutHistory = {};
+  List<Exercise> _currentWorkoutExercises = [];
+  List<CustomExercise> _customExercises = [];
+  int _weeklyWorkoutCount = 0;
+
+  List<Map<String, dynamic>> get weeklyPlan => _weeklyPlan;
   Map<DateTime, List<Exercise>> get workoutHistory => _workoutHistory;
+  List<Exercise> get todaysExercises => _currentWorkoutExercises;
+  List<CustomExercise> get customExercises => _customExercises;
+  int get weeklyWorkoutCount => _weeklyWorkoutCount;
+  int get completedExercisesCount => _currentWorkoutExercises.where((e) => e.isCompleted).length;
+  int get totalExercisesCount => _currentWorkoutExercises.length;
+  double get completionPercentage => totalExercisesCount == 0 ? 0 : (completedExercisesCount / totalExercisesCount) * 100;
 
   Future<void> init() async {
     _dataBox = Hive.box('workout_data');
     _historyBox = Hive.box<List>('workout_history');
     _customExercisesBox = Hive.box<CustomExercise>('custom_exercises');
+
     _weeklyWorkoutCount = _dataBox.get('weeklyWorkoutCount', defaultValue: 0);
     _customExercises = _customExercisesBox.values.toList();
+    
+    // Add default exercises if the library is empty
+    if (_customExercises.isEmpty) {
+      _addDefaultExercises();
+    }
 
-    // This is the new logic to load saved history
-    _workoutHistory = {}; // Clear in-memory map first
+    _workoutHistory = {};
     for (var key in _historyBox.keys) {
       final date = DateTime.parse(key as String);
       final exercises = _historyBox.get(key)!.cast<Exercise>().toList();
@@ -45,5 +89,27 @@ class WorkoutService extends ChangeNotifier {
     }
   }
 
-  // ... (The rest of the service file remains the same)
+  void _addDefaultExercises() {
+    final defaultExercises = [
+        CustomExercise(name: 'Deadlifts', muscleGroup: 'Back'),
+        CustomExercise(name: 'Barbell Incline Bench Press', muscleGroup: 'Chest'),
+        CustomExercise(name: 'Squats', muscleGroup: 'Legs'),
+        CustomExercise(name: 'Barbell Push Press', muscleGroup: 'Shoulders'),
+        CustomExercise(name: 'Lat Pulldowns', muscleGroup: 'Back'),
+        CustomExercise(name: 'Dumbbell Curls', muscleGroup: 'Biceps'),
+    ];
+    for (var ex in defaultExercises) {
+      _customExercisesBox.add(ex);
+    }
+    _customExercises = defaultExercises;
+  }
+  
+  void startWorkoutForDay(DateTime date) { /* ... same as before ... */ }
+  int finishCurrentWorkout() { /* ... same as before ... */ }
+  List<String> getMusclesForDay(DateTime date) { /* ... same as before ... */ }
+  void toggleExerciseCompletion(Exercise exercise) { /* ... same as before ... */ }
+  void updatePlanForDay(String day, List<String> muscles) { /* ... same as before ... */ }
+  List<CustomExercise> getExercisesForMuscleGroups(List<String> muscles) { /* ... same as before ... */ }
+  void addCustomExercise(CustomExercise exercise) { /* ... same as before ... */ }
+  void deleteCustomExercise(CustomExercise exercise) { /* ... same as before ... */ }
 }
