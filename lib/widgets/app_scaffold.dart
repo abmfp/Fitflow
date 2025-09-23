89211f231ebdeeaafc7c26f622e7dd2147cf0054
abmfp <abmfp2@gmail.com>
2025-09-23 15:13:25 +0530

    Create app_scaffold.dart

---
+++ lib/widgets/app_scaffold.dart
@@ -0,0 +1,30 @@
+import 'package:fitflow/widgets/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final PreferredSizeWidget? appBar;
+  final Widget body;
+  final Widget? floatingActionButton;
+  final Widget? bottomNavigationBar;
+
+  const AppScaffold({
+    super.key,
+    this.appBar,
+    required this.body,
+    this.floatingActionButton,
+    this.bottomNavigationBar,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientContainer(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: appBar,
+        body: body,
+        floatingActionButton: floatingActionButton,
+        bottomNavigationBar: bottomNavigationBar,
+      ),
+    );
+  }
+}
