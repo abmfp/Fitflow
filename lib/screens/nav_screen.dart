e6dd2f285fb26b418e4d0e8aef4d2c52d64a2d88
abmfp <abmfp2@gmail.com>
2025-09-23 16:45:57 +0530

    Update nav_screen.dart

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -2,6 +2,7 @@ import 'package:fitflow/screens/home_screen.dart';
 import 'package:fitflow/screens/plan_screen.dart';
 import 'package:fitflow/screens/profile_screen.dart';
 import 'package:fitflow/screens/progress_screen.dart';
+import 'package:fitflow/widgets/ app_scaffold.dart';
 import 'package:flutter/material.dart';
 
 class NavScreen extends StatefulWidget {
