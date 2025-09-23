6517e322ff889636633bedb97e92beb3777119fb
abmfp <abmfp2@gmail.com>
2025-09-23 12:19:32 +0530

    Update workout_detail_screen.dart

--- lib/screens/workout_detail_screen.dart
+++ lib/screens/workout_detail_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
+import 'dart.io';
 import 'package:fitflow/services/workout_service.dart';
-import 'package:fitflow/widgets/gradient_container.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -55,7 +54,7 @@ class _WorkoutDetailScreenState extends State<WorkoutDetailScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: GradientContainer(
+      body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(20.0),
           child: Column(
